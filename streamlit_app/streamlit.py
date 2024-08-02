f3af70bfae75449bc614e728bfa4b24c9d3c3f3e
Madhu <136956190+Madhu1524@users.noreply.github.com>
2024-08-02 11:29:09 +0530

    Update streamlit.py

--- streamlit_app/streamlit.py
+++ streamlit_app/streamlit.py
@@ -11,7 +11,7 @@ import sys
 from pyngrok import ngrok
 
 # Set the ngrok authtoken
-ngrok.set_auth_token('2k0W9k7SfjbnYBuXiuEIZ8qXCBD')
+ngrok.set_auth_token('2k0W9hqUaELaz2cMc8B8R7hcaH9_7SbhoHDWZ9EQ6UKwA9h3g')
 
 # Stop existing tunnels
 ngrok.kill()
